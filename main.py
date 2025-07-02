1d13c77c74d2d230e3f1151fed2ca5009cb55d88
541bot <tobias_richter@outlook.com>
2025-07-02 23:27:39 +0200

    Added uv to the project for better venv handling. Added the asteroids.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -15,8 +17,12 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -31,5 +37,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
